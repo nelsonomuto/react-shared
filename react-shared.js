d7fe534745ee072c0c484255a194a62d07bd1c60
Nelson Omuto <nelsonomuto@gmail.com>
2018-03-20 02:03:34 -0700

    update shared list

--- react-shared.js
+++ react-shared.js
@@ -6,7 +6,13 @@ export const name = 'react-shared';
 import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
 
 checkNpmVersions({
-    'react': '16.x'
+    'react': '16.2.0',
+    'react-dom': '16.2.0',    
+    '@progress/kendo-react-grid': '0.4.0',
+    '@progress/kendo-data-query': '1.2.1',
+    '@progress/kendo-react-inputs': '0.4.0',
+    '@progress/kendo-react-intl': '0.4.0',
+    '@progress/kendo-react-dropdowns': '0.4.0'
 }, 'nomuto:react-shared');
 
 export const List = require('./src/List').default;
--- src/List.js
+++ src/List.js
@@ -4,7 +4,7 @@
 
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import CircularProgress from 'material-ui/CircularProgress';
+import { Grid, GridColumn as Column } from '@progress/kendo-react-grid';
 
 export default class List extends Component {
 
@@ -17,29 +17,19 @@ export default class List extends Component {
         };
     }
 
+    /**
+     * Template method
+     */
+    afterInstantiation() {}
+
+    /**
+     * 
+     * @param {Object} props should have a listData property
+     */
     constructor (props) {
         super(props);
         this.state = Object.assign(this.initState(), props);
-        if (this.state.refreshGrid) this.gridRendered = false;
-    }
-
-    shouldComponentUpdate (prevState, nextState) {
-        if (nextState.hasPermissions === false) {
-            return true;
-        }
-        return !this.gridRendered;
-    }
-
-    addRecord (model) {
-        Meteor.call('templates.insert', model);
-    }
-
-    editRecord (model) {
-        Meteor.call('templates.update', model._id, model);
-    }
-
-    deleteRecord (model) {
-        Meteor.call('templates.remove', model._id, model);
+        this.afterInstantiation(props);
     }
 
     beforeEdit (history, model, onEditEndCallback) {
@@ -49,38 +39,8 @@ export default class List extends Component {
         history.push(`${this.state.editPath}/${id}`);
     }
 
-    onInitGrid () {}
-
-    onLoad (data) {
-        data.map(item => {
-            if (item === 'DENIED') {
-                this.setState({hasPermissions: false});
-            }
-        });
-
-        const templates = getTemplatesFromCollection();
-        const serviceLines = getServiceLinesFromCollection();
-
-        if (!this.gridRendered) {
-            const gridOpts = {
-                el: this.refGrid,
-                data: templates,
-                serviceLineMap: getServiceLineMap(serviceLines),
-                onInit: this.onInitGrid.bind(this),
-                addCallback: this.addRecord.bind(this),
-                editCallback: this.editRecord.bind(this),
-                removeCallback: this.deleteRecord.bind(this),
-                beforeEditCallback: this.beforeEdit.bind(this,
-                    this.props.history)
-            };
-            setTimeout(grid(gridOpts), 1000);
-            this.gridRendered = true;
-        }
-    }
-
     componentDidMount () {
         this.setState({hasPermissions: true});
-        loadGrid(this.onLoad.bind(this));
     }
 
     renderErrorPermissions () {
@@ -92,11 +52,21 @@ export default class List extends Component {
         );
     }
 
+    /**
+     * Template method should be overriden by implementing class
+     */
     renderGridContainer () {
         return (
-            <div className="grid-container" ref={el => (this.refList = el)}>
-                <div ref={el => (this.refGrid = el)}/>
-            </div>
+            <Grid
+                style={{ maxHeight: '400px' }}
+                data={this.state.listData}
+            >
+                <Column field="ProductID" title="ID" width="40px" />
+                <Column field="ProductName" title="Name" width="250px" />
+                <Column field="Category.CategoryName" title="CategoryName" />
+                <Column field="UnitPrice" title="Price" width="80px" />
+                <Column field="UnitsInStock" title="In stock" width="80px" />
+            </Grid>
         );
     }
 
@@ -105,14 +75,6 @@ export default class List extends Component {
             <div>
                 {!this.state.hasPermissions && this.renderErrorPermissions()}
                 {this.state.hasPermissions && this.renderGridContainer()}
-                <CircularProgress className='loading' size={80} thickness={6}
-                                  style={{
-                                      position: 'fixed',
-                                      left: '41%',
-                                      top: '40%'
-                                  }}
-                                  color='white'
-                />
             </div>
         );
     }
